753b7eb505ed0c7cc81a2f0c4efbdfe750377ff2
trf <trf.studios246@gmail.com>
2021-06-19 20:58:46 -0400

    http:added

--- lib/src/services/nominatim.dart
+++ lib/src/services/nominatim.dart
@@ -3,14 +3,15 @@ import 'dart:convert';
 
 class NominatimService {
   Future<List<Map>> getAddressLatLng(String address) async {
+
     var url =
         "https://nominatim.openstreetmap.org/search?q=${address.replaceAll(RegExp(' '), '+')}&format=json&addressdetails=1";
     print(url);
-    var response = await http.get(url);
+    var response = await http.get(Uri.parse(url));
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
     List addresses = jsonDecode(response.body);
-    List<Map> share = List<Map>();
+    List<Map> share = [];
     for (Map ad in addresses) {
       share.add({
         'lat': ad['lat'],
